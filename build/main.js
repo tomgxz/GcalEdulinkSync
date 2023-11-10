328262457b3e77b2bc523b25a3f70a423661fb72
OscarRatcliffe <cyberskely@gmail.com>
2023-11-10 23:13:24 +0000

    Auto skip pre existing events

--- build/main.js
+++ build/main.js
@@ -39,11 +39,25 @@ for (const event of Object.values(events)) {
 // -------------
 // Wait function
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-function createCalendarEvent(auth, data) {
+function createCalendarEvent(auth, data, existingData) {
     return __awaiter(this, void 0, void 0, function* () {
         for (let key in data) {
             const startTime = new Date(data[key].start);
             const endTime = new Date(data[key].end);
+            var timeMatch = false;
+            var matchListPos = 0;
+            for (let i = 0; i < existingData[0].length; i++) {
+                if (existingData[0][i].slice(0, -1) == startTime.toISOString().slice(0, -5)) {
+                    timeMatch = true;
+                    matchListPos = i;
+                }
+            }
+            if (timeMatch) {
+                if (data[key].summary == existingData[1][matchListPos]) {
+                    console.log("Skipped event");
+                    continue;
+                }
+            }
             const event = {
                 summary: data[key].summary,
                 location: data[key].location,
@@ -56,14 +70,41 @@ function createCalendarEvent(auth, data) {
                     timeZone: 'GMT',
                 },
             };
-            google.calendar({ version: 'v3', auth }).events.insert({
-                calendarId: keys.calendarID,
-                resource: event,
-            });
+            // google.calendar({ version: 'v3', auth }).events.insert({
+            //     calendarId: keys.calendarID,
+            //     resource: event,
+            // });
+            console.log(`Created event ${event}`);
             yield delay(100); //Avoid rate limiting (10 requests per second)
         }
     });
 }
 ;
-(0, GcalHandler_1.authorize)().then((auth) => createCalendarEvent(auth, events)).catch(console.error);
+// authorize().then((auth) => createCalendarEvent(auth, events)).catch(console.error);
+// Get a list of all the current events in the google calendar
+function listEvents(auth) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const calendar = google.calendar({ version: 'v3', auth });
+        const eventTimes = [];
+        const eventNames = [];
+        const res = yield calendar.events.list({
+            calendarId: keys.calendarID,
+            timeMin: (new Date()).toISOString(),
+            maxResults: 50,
+            singleEvents: true,
+            orderBy: 'startTime',
+        });
+        const events = res.data.items;
+        events.forEach((event) => {
+            const start = event.start.dateTime || event.start.date;
+            eventTimes.push(start);
+            eventNames.push(event.summary);
+        });
+        return [eventTimes, eventNames];
+    });
+}
+(0, GcalHandler_1.authorize)().then((auth) => __awaiter(void 0, void 0, void 0, function* () {
+    const existingData = yield listEvents(auth);
+    createCalendarEvent(auth, events, existingData);
+})).catch(console.error);
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ const ical = require('ical');
 const fs = require('fs');
 import { getICS } from "./getICS";
 import { authorize } from "./GcalHandler";
+import { calendar } from "googleapis/build/src/apis/calendar";
+import { auth } from "google-auth-library";
 const {google} = require('googleapis');
 
 // -----------------
@@ -42,13 +44,37 @@ for (const event of Object.values(events)) {
 // Wait function
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-async function createCalendarEvent(auth, data) {
+async function createCalendarEvent(auth, data, existingData) {
 
     for (let key in data) {
-      
-        const startTime = new Date(data[key].start);
-        const endTime = new Date(data[key].end);
-    
+        
+        const startTime: Date = new Date(data[key].start);
+        const endTime: Date = new Date(data[key].end);    
+        
+        var timeMatch: boolean = false
+        var matchListPos = 0;
+
+        for (let i = 0; i < existingData[0].length; i++) {
+
+            if (existingData[0][i].slice(0, -1) == startTime.toISOString().slice(0, -5)) {
+
+                timeMatch = true;
+                matchListPos = i
+
+            }
+
+        }
+
+        if (timeMatch) {
+
+            if(data[key].summary == existingData[1][matchListPos]) {
+                
+                console.log("Skipped event")
+                continue;
+
+            }
+        }
+
         const event = {
             summary: data[key].summary,
             location: data[key].location,
@@ -67,9 +93,42 @@ async function createCalendarEvent(auth, data) {
             resource: event,
         });
 
+        console.log(`Created event ${event}`)
+
         await delay(100) //Avoid rate limiting (10 requests per second)
     }
 
 };
 
-authorize().then((auth) => createCalendarEvent(auth, events)).catch(console.error);
+// authorize().then((auth) => createCalendarEvent(auth, events)).catch(console.error);
+
+// Get a list of all the current events in the google calendar
+async function listEvents(auth) {
+
+    const calendar = google.calendar({ version: 'v3', auth });
+
+    const eventTimes = []
+    const eventNames = []
+
+    const res = await calendar.events.list({
+        calendarId: keys.calendarID,
+        timeMin: (new Date()).toISOString(),
+        maxResults: 50,
+        singleEvents: true,
+        orderBy: 'startTime',
+    });
+    
+    const events = res.data.items;
+
+    events.forEach((event) => {
+        const start:Date = event.start.dateTime || event.start.date;
+        eventTimes.push(start)
+        eventNames.push(event.summary)
+    });
+    return [eventTimes, eventNames]
+}
+
+authorize().then(async (auth) => {
+    const existingData = await listEvents(auth);
+    createCalendarEvent(auth, events, existingData);
+}).catch(console.error);
