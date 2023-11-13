b81d3b1cf243ab0748c7e4ca388156eeb36549f0
Oscar <cyberskely@gmail.com>
2023-11-13 09:17:58 +0000

    Added support for multiple calendar downloading

--- src/getICS.ts
+++ src/getICS.ts
@@ -1,11 +1,11 @@
 const https = require('https');
 const fs = require('fs');
 
-function getICS(URL: string): void {
+function getICS(URL: string, Name:String): void {
     
     https.get(URL, (res) => {
 
-        const fileHandler = fs.createWriteStream("data/Download.ICS");
+        const fileHandler = fs.createWriteStream(`data/${Name}Calendar.ICS`);
         res.pipe(fileHandler);
 
         fileHandler.on('finish', () => {
